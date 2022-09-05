846664bbb6606d757354afdccebbc6a071517f96
James Onyekachi <29162889+okilonet1@users.noreply.github.com>
2022-09-05 12:58:19 +0100

    modified:   client/src/App.js 	modified:   client/src/components/Contacts.jsx 	modified:   client/src/index.css 	modified:   client/src/index.js 	modified:   client/src/pages/Login.jsx 	modified:   client/src/pages/Register.jsx 	deleted:    models/userModel.js 	renamed:    models/messageModel.js -> server/models/messageModel.js 	new file:   server/models/userModel.js

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -90,7 +90,7 @@ export default function Register() {
         <form action="" onSubmit={(event) => handleSubmit(event)}>
           <div className="brand">
             <img src={Logo} alt="logo" />
-            <h1>dev chat</h1>
+            <h1>DEV CHAT</h1>
           </div>
           <input
             type="text"
